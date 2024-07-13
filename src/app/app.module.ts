c0d378ffada0ab965a738eae99264086af926e3c
TONG77777 <78796067+TONG77777@users.noreply.github.com>
2024-07-13 13:17:55 +0800

    create nav (#12)
    - move nav as new component
    - create component module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
+import { ComponentModule } from "./component/component.module";
 
 @NgModule({
-  declarations: [AppComponent,HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
