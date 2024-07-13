27cb2454d6b0b36ca8d27f9e1151d7f4de9bd872
TONG77777 <78796067+TONG77777@users.noreply.github.com>
2024-07-13 15:44:53 +0800

    create footer (#19)
    - create new footer component

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 import { NavComponent } from "./nav/nav.component";
 import { CardProfileComponent } from "./card-profile/card-profile.component";
-import { AnnouncementComponent } from "./announcement/announcement.component";
 import { CardComponent } from "./card/card.component";
+import { AnnouncementComponent } from "./announcement/announcement.component";
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     HeaderComponent,
+    FooterComponent,
     NavComponent,
     CardProfileComponent,
     CardComponent,
