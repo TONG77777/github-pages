37cf3a640ffc357a3f984b903ffabad3474032cf
TONG77777 <78796067+TONG77777@users.noreply.github.com>
2024-07-13 14:48:14 +0800

    Design : create announcement (#14)
    - create new announcement component

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from "./nav/nav.component";
 import { CardProfileComponent } from "./card-profile/card-profile.component";
+import { AnnouncementComponent } from "./announcement/announcement.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent, NavComponent, CardProfileComponent],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    CardProfileComponent,
+    AnnouncementComponent,
+  ],
   exports: [HeaderComponent, NavComponent],
 })
 export class ComponentModule {}
